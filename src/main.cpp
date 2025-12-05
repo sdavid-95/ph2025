baa0006d128dcf52ad489914e4ed7ce165b013ed
Dutzu05 <vladlusca05@gmail.com>
2025-12-05 23:30:54 +0200

    humidity

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,13 @@
 
 #define DHTPIN 1       // GPIO 1 on ESP32-C3 Super Mini
 #define DHTTYPE DHT22  // Sensor type
+#define HUMIDITY_PIN 0 
 
 DHT dht(DHTPIN, DHTTYPE);
 
 // Function declaration
 void readDHT();
+void readAnalogHumidity();
 
 void setup() {
   Serial.begin(115200);
@@ -34,4 +36,16 @@ void readDHT() {
   Serial.print(" %  |  Temperature: ");
   Serial.print(t);
   Serial.println(" °C");
+}
+
+void readAnalogHumidity() {
+    int raw = analogRead(HUMIDITY_PIN);   // Read ADC (0–4095 on ESP32-C3)
+    
+    float voltage = (raw / 4095.0) * 3.3;
+    
+    Serial.print("Humidity Sensor Raw: ");
+    Serial.print(raw);
+    Serial.print(" | Voltage: ");
+    Serial.print(voltage);
+    Serial.println(" V");
 }
